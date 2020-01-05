362cb061f55b0d641f7f68857774b71d309ffc67
Ne4istii <ne4istii@gmail.com>
2020-01-05 14:33:41 +0300

    step 3 update func

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
 
-import userName from '..';
-
-
+import actual from '..';
+actual();
--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import readlineSync from 'readline-sync';
-console.log('Welcome to the BrainGames!\n');
+
+/*console.log('Welcome to the BrainGames!\n');
 const userName = readlineSync.question('May I have your name? ');
 console.log('Hello, ' + userName + '!');
+*/
+
+const actual = () => {
+	console.log('Welcome to the BrainGames!\n');
+	const userName = readlineSync.question('May I have your name? ');
+	console.log('Hello, ' + userName + '!');
+};
+
+export default actual;
