ffbba369490b1d59ea57e29e7d16a8d03f64d642
Ne4istii <ne4istii@gmail.com>
2020-01-11 16:16:23 +0300

    update after lint

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import readlineSync from 'readline-sync';
 
-/*console.log('Welcome to the BrainGames!\n');
-const userName = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + userName + '!');
-*/
-
 const actual = () => {
-	console.log('Welcome to the BrainGames!\n');
-	const userName = readlineSync.question('May I have your name? ');
-	console.log('Hello, ' + userName + '!');
+  console.log('Welcome to the BrainGames!\n');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log('Hello, ', userName, '!');
 };
 
 export default actual;
