b55b09586a9e7da045e1d65f0152e9adf34f9d68
Ne4istii <ne4istii@gmail.com>
2020-01-25 19:58:18 +0300

    commit

---
+++ src/bin/brain-calc.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { calc } from '..';
+
+console.log('Welcome to the BrainGames!\n What is the result of the expression?\n');
+calc();
--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import readlineSync from 'readline-sync';
 
 const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
+const randomSym = (len, str) => {
+  let ans = '';
+  for (let i = len; i > 0; i -= 1) {
+    ans += str[Math.floor(Math.random() * str.length)];
+  }
+  return ans;
+};
+
 const yourName = () => readlineSync.question('May I have your name? ');
 
 const printGreeting = () => {
@@ -28,4 +36,32 @@ const evenParity = () => {
   console.log('Congratulations, ', userName, '!');
 };
 
-export { printGreeting, evenParity };
+const calc = () => {
+  let countCorrectAnswer = 0;
+  let correctAnswer = 0;
+  const mathOperator = '*-+';
+  const userName = yourName();
+  console.log('Hello, ', userName, '!\n');
+  while (countCorrectAnswer < 3) {
+    const rInt1 = randomInt(1, 100);
+    const rInt2 = randomInt(1, 100);
+    const rOper = randomSym(1, mathOperator);
+    console.log('Question: ', rInt1, rOper, rInt2);
+    if (rOper === '*') {
+      correctAnswer = rInt1 * rInt2;
+    } else if (rOper === '+') {
+      correctAnswer = rInt1 + rInt2;
+    } else correctAnswer = rInt1 - rInt2;
+    const getAnswer = readlineSync.question('Your answer: ');
+    if (correctAnswer === Number(getAnswer)) {
+      console.log('Correct!');
+      countCorrectAnswer += 1;
+    } else {
+      console.log(Number(getAnswer), 'is wrong answer ;(. Correct answer was', correctAnswer, 'Lets try again, ', userName, '!');
+      countCorrectAnswer = 0;
+    }
+  }
+  console.log('Congratulations, ', userName, '!');
+};
+
+export { printGreeting, evenParity, calc };
