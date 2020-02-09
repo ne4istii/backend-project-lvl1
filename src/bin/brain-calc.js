300d283e1ecad4be3f204464d823175aefa33c9b
Ne4istii <ne4istii@gmail.com>
2020-02-09 14:14:17 +0300

    master

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -3,5 +3,5 @@
 import { calc } from '..';
 
 console.log('Welcome to the BrainGames!\n What is the result of the expression?\n');
-
-calc();
+const mathOperator = '*-+';
+calc(mathOperator, 1, 100, 3);
--- src/index.js
+++ src/index.js
@@ -8,36 +8,49 @@ const randomSym = (str) => str[Math.floor(Math.random() * str.length)];
 
 // Функция сравнения правильного ответа и ответа, полученного от пользователя
 const compareAnswers = (counter, userName, getAnswer, correctAnswer) => {
+  let rCounter;
   if (correctAnswer === Number(getAnswer)) {
     console.log('Correct!');
-    counter += 1;
+    rCounter = counter + 1;
   } else {
     console.log(Number(getAnswer), 'is wrong answer ;(. Correct answer was', correctAnswer, 'Lets try again, ', userName, '!');
-    counter = 0;
+    rCounter = 0;
   }
-  return counter;
+  return rCounter;
 };
 
 // Функция сравнения правильного ответа и ответа, полученного от пользователя
 const compareWordAnswers = (counter, userName, getAnswer, correctAnswer) => {
+  let rCounter;
   if (correctAnswer === (getAnswer === 'yes')) {
     console.log('Correct!');
-    counter += 1;
+    rCounter = counter + 1;
   } else {
     console.log(getAnswer, 'is wrong answer ;(. Correct answer was "no". Lets try again, ', userName, '!');
-    counter = 0;
+    rCounter = 0;
   }
-  return counter;
+  return rCounter;
 };
-
 
 // Фунцикция вычисления НОД
 const calcNod = (a, b) => {
+  let rA; let rB;
   while (a !== 0 && b !== 0) {
-    if (a > b) a %= b;
-    else b %= a;
+    if (a > b) rA = a % b;
+    else rB = b % a;
   }
-  return a + b;
+  return rA + rB;
+};
+
+// Функция вычисления математических операций
+const calcFunc = (rOper, rInt1, rInt2) => {
+  let trueAnswer = 0;
+  if (rOper === '*') {
+    trueAnswer = rInt1 * rInt2;
+  } else if (rOper === '+') {
+    trueAnswer = rInt1 + rInt2;
+  } else trueAnswer = rInt1 - rInt2;
+  return trueAnswer;
 };
 
 // Функция-генератор арифметической прогрессии с заменой одного элемента
@@ -88,29 +101,17 @@ export const evenParity = () => {
   console.log('Congratulations, ', userName, '!');
 };
 
-export const calc = () => {
-  let countCorrectAnswer = 0;
-  let correctAnswer = 0;
-  const mathOperator = '*-+';
+export const calc = (mathOperator, minInt, maxInt, correctAnswerСounter) => {
+  let counter = 0;
   const userName = printGreeting();
-  while (countCorrectAnswer < 3) {
-    const rInt1 = randomInt(1, 100);
-    const rInt2 = randomInt(1, 100);
+  while (counter < correctAnswerСounter) {
+    const rInt1 = randomInt(minInt, maxInt);
+    const rInt2 = randomInt(minInt, maxInt);
     const rOper = randomSym(mathOperator);
     console.log('Question: ', rInt1, rOper, rInt2);
-    if (rOper === '*') {
-      correctAnswer = rInt1 * rInt2;
-    } else if (rOper === '+') {
-      correctAnswer = rInt1 + rInt2;
-    } else correctAnswer = rInt1 - rInt2;
+    const correctAnswer = calcFunc(rOper, rInt1, rInt2);
     const getAnswer = readlineSync.question('Your answer: ');
-    if (correctAnswer === Number(getAnswer)) {
-      console.log('Correct!');
-      countCorrectAnswer += 1;
-    } else {
-      console.log(Number(getAnswer), 'is wrong answer ;(. Correct answer was', correctAnswer, 'Lets try again, ', userName, '!');
-      countCorrectAnswer = 0;
-    }
+    counter = compareAnswers(counter, userName, getAnswer, correctAnswer);
   }
   console.log('Congratulations, ', userName, '!');
 };
