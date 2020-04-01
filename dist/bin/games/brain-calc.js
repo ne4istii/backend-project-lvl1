f647f46ccc4c3d24e6a53d410124cab2ea660973
Ne4istii <ne4istii@gmail.com>
2020-04-01 14:50:19 +0300

    master

--- dist/bin/games/brain-calc.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-
-var _ = require("..");
-
-const mathOperator = '*-+';
-(0, _.calc)(mathOperator, 1, 100, 3);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NyYy9iaW4vZ2FtZXMvYnJhaW4tY2FsYy5qcyJdLCJuYW1lcyI6WyJtYXRoT3BlcmF0b3IiXSwibWFwcGluZ3MiOiJBQUFBOzs7QUFFQTs7QUFFQSxNQUFNQSxZQUFZLEdBQUcsS0FBckI7QUFDQSxZQUFLQSxZQUFMLEVBQW1CLENBQW5CLEVBQXNCLEdBQXRCLEVBQTJCLENBQTNCIiwic291cmNlc0NvbnRlbnQiOlsiIyEvdXNyL2Jpbi9lbnYgbm9kZVxuXG5pbXBvcnQgeyBjYWxjIH0gZnJvbSAnLi4nO1xuXG5jb25zdCBtYXRoT3BlcmF0b3IgPSAnKi0rJztcbmNhbGMobWF0aE9wZXJhdG9yLCAxLCAxMDAsIDMpO1xuIl19
